abf404ad554bdd3e3634f8a47fe9768c84ceb90c
Robin <robinnem@outlook.com>
2025-11-05 16:59:19 -0500

    Added Button & Nav Animations

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const loading = document.querySelector(".btn__overlay--loading");
+const success = document.querySelector(".btn__overlay--success");
+
 function openMenu() {
   document.body.classList += " menu--open";
 }
 
 function closeMenu() {
   document.body.classList.remove("menu--open");
+}
+
+function searchPending() {
+  loading.classList += " btn__overlay--visible";
+  setTimeout(() => {
+    loading.classList.remove("btn__overlay--visible");
+    success.classList += " btn__overlay--visible";
+  }, 1000);
 }
